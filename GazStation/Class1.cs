7dbfc7e9951cf9b8e05e9e32d74bf81a03e03dc3
Valtear <larionov.alex93@gmail.com>
2023-05-29 12:59:01 +0300

    Добавьте файлы проекта.

---
+++ GazStation/Class1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace GazStation
+{
+    internal class CustomDBConnection
+    {
+        string connectionString;
+        string query;
+        SqlConnection connection;
+        SqlCommand command;
+        SqlDataReader reader;
+
+        public CustomDBConnection()
+        {
+            this.connectionString = null;
+            this.query = null;
+            this.connection = null;
+            this.command = null;
+            this.reader = null;
+        }
+
+        public string ConnectionString
+        {
+            get { return this.connectionString; }
+            set
+            {
+                this.connectionString = value;
+                this.connection = new SqlConnection(value);
+            }
+        }
+        public string Query
+        {
+            get { return this.query; }
+            set
+            {
+                this.query = value;
+                this.command = new SqlCommand(value, this.connection);
+            }
+        }
+        public SqlDataReader Reader
+        {
+            get { return this.reader; }
+        }
+        public void Execute()
+        {
+            this.reader = command.ExecuteReader();
+        }
+        public void Open()
+        {
+            connection.Open();
+        }
+        public void Close()
+        {
+            connection.Close();
+        }
+        public bool Read()
+        {
+            return this.reader.Read();
+
+        }
+
+    }
+}
---
+++ GazStation/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GazStation
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        CustomDBConnection myConnect = new CustomDBConnection();
+        public MainWindow()
+        {
+            InitializeComponent();
+            myConnect.ConnectionString = @"Data Source=DESKTOP-CSA0P33\SQLEXPRESS;Initial Catalog=Gaz_Station;Integrated Security=True";
+        }
+
+        private void btn_auth_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                myConnect.Query = "select * from Users";
+                myConnect.Open();
+                myConnect.Execute();
+                while (myConnect.Read())
+                {
+                    if (Convert.ToInt32(myConnect.Reader[5]) == 1)
+                    {
+                        MessageBox.Show("Admin");
+                    }
+                    else
+                    {
+                        MessageBox.Show("User");
+                    }
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Error");
+            }
+            finally
+            {
+                myConnect.Close();
+            }
+        }
+    }
+}
